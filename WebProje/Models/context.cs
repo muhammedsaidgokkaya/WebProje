76372567c1d9204e18600f3195599d7cf171809c
sainttrigger <erentetik545454@gmail.com>
2022-11-20 20:52:51 +0300

    kod yazdıkm

---
+++ WebProje/Models/context.cs
@@ -0,0 +1,22 @@
+﻿using static WebProje.Models.context;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebProje.Models
+{
+    public class context:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost; user=root; database=Category; password=GOKKAYA52.o; port=3306");
+        }
+        /// <summary>
+        /// eren tetikk
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Urunler> Urunlers { get; set; }
+        public DbSet<AdminUser> AdminUsers { get; set; }
+
+    }
+}
